5b29f7690b85f520ab5d97c715eed55c6298bb82
jbadeau <jose.badeau@gmail.com>
2014-03-31 22:44:23 +0200

    updated grammer api

--- example/spec/LogService/LogServiceSpec.js
+++ example/spec/LogService/LogServiceSpec.js
@@ -1,47 +1,59 @@
+/**
+ * An example behave specification.
+ */
+
+/**
+ * 1. Start the spec with a given name.
+ */
 behave.spec('LogService')
 
-// wire 1-n context files like
-// ('context') or ({}) or ['context', {}, ...]
-// load factories, services, mock objects, ...
-.wire('LogService')
+/**
+ * 2. load 1-n wire context files and/or object literals.
+ * <p>
+ * ('context') or ({}) or ['context', {}, ...]
+ * <p>
+ * The context lives throughout the duration of the spec and it's properties can
+ * be accessed in the spec using the autowire annotation.
+ */
+.wire([ './root' ])
 
-// observable data which can be weaved and used in expect assertions
+/**
+ * 3. Declare data properties which are made implicity available.
+ * <p>
+ * Is there some usecase for observables here
+ */
 .data({
 	logService : null,
-	logListener : observer(null)
+	logListener : null
 })
 
-// setup -> weave
-// inject wire components using @Autowire
-// all methods support returning promises for async support
+/**
+ * 4. Standard setup methods.
+ * <p>
+ * Observables are implicity available.
+ * <p>
+ * Use Autowire the to inject wire components.
+ */
 .before({
-	all : {
-		setup : function() {
-			console.info('setup called once before all tests');
-		},
-		weave : function() {
-			console.info('weave called once before all tests');
-		}
+	all : function() {
+		console.info('setup called once before all tests');
 	},
-	each : {
-		setup : function(/* @Autowire */logFactory) {
-			console.info('setup called before each tests');
-			this.logService.set(logFactory.newInstance());
-			this.logListener.set(function(logEntry) {
-			});
-		},
-		weave : function() {
-			console.info('weave called before each tests');
-			// .advise uses meld and tracks removers for automatic removal
-			advise.on(this.logFactory.log, onFunction);
-		}
+	// inject logFactory from wire context
+	each : function(/* @Autowire */logFactory) {
+		console.info('setup called before each tests');
+		this.logService.set(logFactory.newInstance());
+		// sinon spy
+		this.logListener = spy();
 	}
 })
 
-// properties are available via this
-// setup -> weave
-// inject wire components using @Autowire
-// all methods support returning promises for async support
+/**
+ * 5. Standard teardown methods.
+ * <p>
+ * Observables are implicity available.
+ * <p>
+ * Use Autowire the to inject wire components.
+ */
 .after({
 	all : {
 		teardown : function() {
@@ -54,47 +66,72 @@ behave.spec('LogService')
 	}
 })
 
-// - properties are available via this
-// - inject wire components in in methods using @Autowire
-// - all methods support returning promises for async support
-// - assertions are provided by sinon and chai adapters
-// - so.expect takes either a settle resolver (function(descriptor ){}) or a non function ref or empty()
+/**
+ * Describe tests.
+ * <p>
+ * Observables are implicity available
+ * <p>
+ * Use Autowire the to inject wire components.
+ * <p>
+ * it methods support returning promises
+ * <p>
+ * expect methods support either a settle resolver function, a value or empty()
+ * <p>
+ * assert expectations using chai + sinon assertions
+ */
 .describe('.log', function() {
 
-	// resolve
+	// basic resolve expectation. An exception or rejected promise fails test.
 	it('should not fail', function() {
 		this.logService(INFO, 'i am a sweet logger');
 	}).so.expect().to.eventually.resolve();
-	
-	// reject
+
+	// basic reject expectation. Anything but an exception or rejected promise
+	// fails the test
 	it('should fail when called with an invalid log level', function() {
 		this.logService(undefined, 'i am a naughty logger');
 	}).so.expect().to.eventually.reject();
-	
-	// expect some non function ref
+
+	// expect that the logListener (sinon spy) is called with some data
 	it('should notify log listeners', function() {
 		this.logService(INFO, 'i am a sweet logger');
-	}).so.expect(this.logListener).to.eventually.be.calledWith({
+	})
+	so.expect(this.logListener).to.eventually.be.calledWith({
 		level : 1,
 		message : 'i am a sweet logger'
 	});
 
-	// expect settle resolver
+	// expect that the resolved value or promise contains
 	it('should fail when called with an invalid log level', function() {
 		this.logService(undefined, 'i am a naughty logger');
 	}).so.expect(function(descriptor) {
-		return descriptor.state;
-	}).to.eventually.equal('rejected');
-	
+		return descriptor.reason;
+	}).to.eventually.equal('invalid log level');
 
 })
 
 // - nesting support
-.describe('.someFunction', function() {
+.describe('.someLogFunction', function() {
+
+	// Wire child context that will be automatically destroyed on exit of
+	// describe.
+	wire('child')
+
+	// Mixin child properties that will be automatically removed on exit of
+	// describe. Properties will shade (not replace) same named parent
+	// properties.
+	.data()
+
+	//
+	.before()
+
+	//
+	.after();
+
 	describe('given some context', function() {
-		it('should do something', function(/* @Autowire */foo) {
-			return foo();
-		}).so.expect().to.equal('bar');
+		it('should do something', function() {
+			return this.logService.foo();
+		}).so.expect().to.eventually.equal('bar');
 	});
 })
 
